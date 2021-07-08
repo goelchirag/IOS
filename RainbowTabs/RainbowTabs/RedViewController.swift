c24ff15117d3e30737de3777a13f2b11761d79c9
goelchirag <55796593+goelchirag@users.noreply.github.com>
2021-07-08 20:46:13 +0530

    Add files via upload

---
+++ RainbowTabs/RainbowTabs/RedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by Angered Force on 4/14/1400 AP.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tabBarItem.badgeValue =  nil
+    }
+
+
+}
+
